9455fa4c689a448f2484dff3f6a110c7a9b76535
Jeff Navarra <jeff.navarra@miapeer.com>
2023-01-09 17:45:39 -0700

    Update layout

--- miapeer/src/routes/+layout.server.ts
+++ miapeer/src/routes/+layout.server.ts
@@ -1,8 +1,12 @@
+import { PUBLIC_MIAPEER_API_HOST } from '$env/static/public';
 import * as jose from 'jose'
+import { env } from '$env/dynamic/private';
 
 /** @type {import('./$types').LayoutServerLoad} */
 export async function load({cookies}) {
     let accessToken = cookies.get("MAT");
+    const miapeerApiBase = `${PUBLIC_MIAPEER_API_HOST}/miapeer/v1`
+
     let userName;
 
     try {
@@ -14,9 +18,12 @@ export async function load({cookies}) {
         userName = null
     }
 
+    // TODO: Is this ok, or does it share with other users?
+    env['miapeerApiBase'] = miapeerApiBase
+    env['accessToken'] = accessToken
+
     return {
         isAuthenticated: !!accessToken,
-        accessToken,
         userName,
     };
 }
