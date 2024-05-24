b8b9a23d52a983447191c7f15c7880920b8017d9
Jep <jeff.navarra@miapeer.com>
2024-05-24 16:40:57 -0600

    Fix homepage

--- miapeer/src/routes/+page.svelte
+++ miapeer/src/routes/+page.svelte
@@ -1,7 +1,5 @@
-<script>
-    import Applications from './Applications.svelte';
-
-    /** @type {import('./$types').PageData} */
+<script lang="ts">
+    import type { PageData } from './$types';
     export let data;
 </script>
 
@@ -9,4 +7,26 @@
     <title>Miapeer: Home</title>
 </svelte:head>
 
-<Applications applications={data.applications} />
+<div class="container h-full mx-auto flex justify-center items-center">
+    {#each data.applications as application}
+        <a href={application.url} class="card card-hover p-4 w-80 m-5 text-center">
+            <header class="card-header">
+                <i class={`icon bg-primary-500 ${application.icon}`} />
+            </header>
+            <section class="p-4 text-4xl font-bold">{application.name}</section>
+            <footer class="card-footer">{@html application.description}</footer>
+        </a>
+    {/each}
+</div>
+
+<style type="postcss">
+    .card .icon {
+        border-radius: 100%;
+        font-size: 3rem;
+        text-align: center;
+        line-height: 4rem;
+        height: 4rem;
+        width: 4rem;
+        text-indent: 4px;
+    }
+</style>
