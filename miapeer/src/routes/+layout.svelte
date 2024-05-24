c02319621bfb35a9f278d2e023717cb123cfc1f4
Jep <jeff.navarra@miapeer.com>
2024-05-24 16:39:09 -0600

    Fix layout

--- miapeer/src/routes/+layout.svelte
+++ miapeer/src/routes/+layout.svelte
@@ -1,49 +1,82 @@
-<script type="typescript">
-    import './styles.css';
-
-    import Link from '$lib/Link.svelte';
+<script lang="ts">
+    import '../app.postcss';
+    import { AppShell, AppBar } from '@skeletonlabs/skeleton';
     import logoLong from '$lib/images/miapeer-logo-long.svg';
-    import LogInButton from '../routes/login/LogInButton.svelte';
-    import LogOutButton from '../routes/logout/LogOutButton.svelte';
+
+    // Highlight JS
+    import hljs from 'highlight.js/lib/core';
+    import 'highlight.js/styles/github-dark.css';
+    import { storeHighlightJs } from '@skeletonlabs/skeleton';
+    import xml from 'highlight.js/lib/languages/xml'; // for HTML
+    import css from 'highlight.js/lib/languages/css';
+    import javascript from 'highlight.js/lib/languages/javascript';
+    import typescript from 'highlight.js/lib/languages/typescript';
+
+    hljs.registerLanguage('xml', xml); // for HTML
+    hljs.registerLanguage('css', css);
+    hljs.registerLanguage('javascript', javascript);
+    hljs.registerLanguage('typescript', typescript);
+    storeHighlightJs.set(hljs);
+
+    // Floating UI for Popups
+    import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
+    import { storePopup } from '@skeletonlabs/skeleton';
+    storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 
     import type { LayoutData } from './$types';
     export let data: LayoutData;
+
+    import { goto, invalidateAll } from '$app/navigation';
+
+    const handleLogout = async () => {
+        const res = await fetch('/logout', {
+            method: 'POST'
+        });
+
+        if (res.ok) {
+            const data = await res.json();
+            invalidateAll();
+            goto(data.redirectUrl ?? '/');
+        } else {
+            console.error('NOT ok');
+        }
+    };
 </script>
 
-<svelte:head>
-    <meta
-        name="description"
-        content="Welcome to Miapeer, the home of Quantum. How can I be of assistance?"
-    />
-    <meta name="author" content="Jeffery Navarra" />
-    <meta name="mobile-web-app-capable" content="yes" />
+<AppShell>
+    <svelte:fragment slot="header">
+        <AppBar>
+            <svelte:fragment slot="lead">
+                <a href="/" alt="Logo"><img src={logoLong} class="logo-img" alt="Logo" /></a>
+            </svelte:fragment>
+            <svelte:fragment slot="trail">
+                {#if data.isAuthenticated}
+                    <a class="btn btn-sm variant-ghost-surface" href="/quantum"> Quantum </a>
+                    <a class="btn btn-sm variant-ghost-surface" href="/users"> User Management </a>
+                    <button class="btn btn-sm variant-ghost-surface" on:click={handleLogout}>
+                        Log Out
+                    </button>
+                {:else}
+                    <a class="btn btn-sm variant-ghost-surface" href="/login">
+                        Log In / Register
+                    </a>
+                {/if}
+            </svelte:fragment>
+        </AppBar>
+    </svelte:fragment>
 
-    <link
-        rel="stylesheet"
-        href="https://fonts.googleapis.com/css?family=Lato:300,400,700%7CMontserrat:400,700%7CRaleway:300,400,600"
-    />
-</svelte:head>
+    <slot />
 
-<div class="app">
-    <header>
-        <Link href="/"><img src={logoLong} class="logo-img long" alt="Logo" /></Link>
+    <svelte:fragment slot="footer">
+        <AppBar
+            gridColumns="grid-cols-3"
+            slotDefault="place-self-center"
+            slotTrail="place-content-end"
+        >
+            <svelte:fragment slot="lead">
+                <span>&copy; 2015-{new Date().getFullYear()}, Miapeer LLC</span>
+            </svelte:fragment>
 
-        {#if data.isAuthenticated}
-            <Link href="/quantum">Quantum</Link>
-            <Link href="/users">User Management</Link>
-            <LogOutButton userName={data.userName} />
-        {:else}
-            <LogInButton />
-        {/if}
-    </header>
-
-    <div class="main-and-footer">
-        <main>
-            <slot />
-        </main>
-
-        <footer>
-            <span>&copy; 2015-{new Date().getFullYear()}, Miapeer LLC</span>
             {#if data.lastUpdate}
                 <span
                     >Updated: {data.lastUpdate === 'dev'
@@ -51,69 +84,20 @@
                         : new Date(data.lastUpdate).toLocaleString()}</span
                 >
             {/if}
-            <Link href="/portfolio">Jeff Navarra's Portfolio</Link>
-        </footer>
-    </div>
-</div>
 
-<style>
-    .app {
-        height: 100vh;
-        height: 100dvh;
+            <svelte:fragment slot="trail">
+                <a class="anchor" href="/portfolio"> Jeff Navarra's Portfolio </a>
+            </svelte:fragment>
+        </AppBar>
+    </svelte:fragment>
+</AppShell>
 
-        display: grid;
-        grid-template-columns: 1fr;
-        grid-template-rows: auto 1fr;
-        grid-template-areas:
-            'header'
-            'main-and-footer';
-    }
-
-    header {
-        grid-area: header;
-        background-color: rgba(var(--bg-primary-rgb), 0.9);
-
-        box-shadow: 0 0 1em black;
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        padding: 0.25em 1em;
-        text-transform: uppercase;
-
-        z-index: 1;
-    }
-
+<style type="postcss">
     .logo-img {
-        background-color: var(--theme-primary);
+        @apply bg-primary-500;
         border: 1px solid black;
         border-radius: 1em;
         height: 3.5em;
         padding: 0.5em;
-    }
-
-    .main-and-footer {
-        grid-area: main-and-footer;
-        overflow: auto;
-
-        display: grid;
-        grid-template-rows: 1fr auto;
-        grid-template-columns: 1em 1fr 1em;
-        grid-template-areas:
-            'gutter-left main gutter-right'
-            'footer footer footer';
-    }
-
-    main {
-        grid-area: main;
-    }
-
-    footer {
-        grid-area: footer;
-        color: var(--text-primary-negative);
-        background-color: rgba(var(--bg-primary-negative-rgb), 0.8);
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        padding: 0 1em;
     }
 </style>
