dbb0a33fbc4592ded7d8e2f214a911b2e7655e34
Jep <jeff.navarra@miapeer.com>
2024-09-04 18:38:37 -0600

    Move transaction settings to drawer

--- miapeer/src/routes/quantum/QuantumPage.svelte
+++ miapeer/src/routes/quantum/QuantumPage.svelte
@@ -1,14 +1,15 @@
 <script lang="ts">
     import MiapeerPage from '../MiapeerPage.svelte';
-    import { popup } from '@skeletonlabs/skeleton';
+    import { popup, Drawer, getDrawerStore } from '@skeletonlabs/skeleton';
     import { page } from '$app/stores';
-    import { goto, invalidate } from '$app/navigation';
 
     import type { PageData } from './$types';
 
     export let data: PageData;
     export let pageTitle = 'Quantum';
     export let headline = '';
+
+    const drawerStore = getDrawerStore();
 
     let futureMarker = $page.url.searchParams.get('marker') ?? 7;
     let lowBalanceThreshold = $page.url.searchParams.get('threshold') ?? 250;
@@ -18,56 +19,61 @@
     const updateLimits = async () => {
         window.location = `/quantum/accounts/${$page.params.accountId}/transactions?marker=${futureMarker}&threshold=${lowBalanceThreshold}&limitmonths=${transactionMonthLimit}&forecastmonths=${forecastMonthLimit}`;
     };
+
+    const closeSettingsDrawer = () => {
+        drawerStore.close();
+    };
 </script>
 
 <MiapeerPage {pageTitle} {headline} {data}>
-    <svelte:fragment slot="header">
-        <div class="grid grid-cols-[1fr_14rem_18rem_13rem_17rem_12rem_12rem] gap-4">
-            <div></div>
-            <div>
-                {#if $page.url.pathname === `/quantum/accounts/${$page.params.accountId}/transactions`}
-                    <div class="input-group input-group-divider grid-cols-[11rem_auto]">
+    <svelte:fragment slot="settingsDrawer">
+        <Drawer position="top">
+            {#if $page.url.pathname === `/quantum/accounts/${$page.params.accountId}/transactions`}
+                <div class="grid grid-cols-[24rem] gap-4">
+                    <div class="input-group input-group-divider grid-cols-[14rem_auto]">
                         <div class="input-group-shim">Mark future days</div>
-                        <input type="text" bind:value={futureMarker} on:focusout={updateLimits} />
+                        <input type="text" bind:value={futureMarker} />
                     </div>
-                {/if}
-            </div>
-            <div>
-                {#if $page.url.pathname === `/quantum/accounts/${$page.params.accountId}/transactions`}
                     <div class="input-group input-group-divider grid-cols-[14rem_auto]">
                         <div class="input-group-shim">Low balance threshold</div>
-                        <input
-                            type="text"
-                            bind:value={lowBalanceThreshold}
-                            on:focusout={updateLimits}
-                        />
+                        <input type="text" bind:value={lowBalanceThreshold} />
                     </div>
-                {/if}
-            </div>
-            <div>
-                {#if $page.url.pathname === `/quantum/accounts/${$page.params.accountId}/transactions`}
-                    <div class="input-group input-group-divider grid-cols-[10rem_auto]">
+                    <div class="input-group input-group-divider grid-cols-[14rem_auto]">
                         <div class="input-group-shim">Months of data</div>
-                        <input
-                            type="text"
-                            bind:value={transactionMonthLimit}
-                            on:focusout={updateLimits}
-                        />
+                        <input type="text" bind:value={transactionMonthLimit} />
                     </div>
-                {/if}
-            </div>
-            <div>
-                {#if $page.url.pathname === `/quantum/accounts/${$page.params.accountId}/transactions`}
                     <div class="input-group input-group-divider grid-cols-[14rem_auto]">
                         <div class="input-group-shim">Months of forecast data</div>
-                        <input
-                            type="text"
-                            bind:value={forecastMonthLimit}
-                            on:focusout={updateLimits}
-                        />
+                        <input type="text" bind:value={forecastMonthLimit} />
                     </div>
-                {/if}
-            </div>
+
+                    <div class="grid grid-cols-[1fr_1fr] gap-4">
+                        <button
+                            type="button"
+                            class="btn variant-ghost-surface"
+                            on:click={closeSettingsDrawer}
+                        >
+                            Cancel
+                        </button>
+
+                        <button
+                            type="button"
+                            class="btn variant-filled-primary"
+                            on:click={updateLimits}
+                        >
+                            {'Save Settings'}
+                        </button>
+                    </div>
+                </div>
+            {:else}
+                <div>No settings preset for this page</div>
+            {/if}
+        </Drawer>
+    </svelte:fragment>
+
+    <svelte:fragment slot="header">
+        <div class="grid grid-cols-[1fr_12rem_12rem] gap-4">
+            <div></div>
             <div>
                 {#if data.accounts.length}
                     <button
