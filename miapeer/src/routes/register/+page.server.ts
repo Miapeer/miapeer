546767e08dce3dcb9813e64b0f0fac44cdb335d9
Jep <jeff.navarra@miapeer.com>
2024-09-12 14:09:30 -0600

    Add server method to create user

--- miapeer/src/routes/register/+page.server.ts
+++ miapeer/src/routes/register/+page.server.ts
@@ -9,9 +9,33 @@ export const load: PageServerLoad = async ({ locals }) => {
 };
 
 export const actions: Actions = {
-    default: async ({ request }) => {
+    default: async ({ request, locals }) => {
         const data = await request.formData();
         const email = data.get('email');
         const password = data.get('password');
+
+        const requestData = {
+            email,
+            password,
+            disabled: false
+        };
+
+        const response = await fetch(`${locals.app.miapeerApiBase}/users`, {
+            method: 'POST',
+            headers: {
+                accept: 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(requestData)
+        });
+
+        const responseData = await response.json();
+
+        if (!response.ok) {
+            console.error(response);
+            return fail(401, responseData);
+        }
+
+        redirect(303, '/login');
     }
 };
