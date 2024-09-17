e630f1f0141e77c877042f8b99a36738953a0795
Jep <jeff.navarra@miapeer.com>
2024-09-17 15:19:41 -0600

    Move popups to layout

--- miapeer/src/routes/quantum/QuantumPage.svelte
+++ miapeer/src/routes/quantum/QuantumPage.svelte
@@ -103,36 +103,6 @@
                 </button>
             </div>
         </div>
-
-        <div data-popup="accountPopupCombobox">
-            <div class="btn-group-vertical variant-filled">
-                <a href="/quantum/accounts">Manage Accounts</a>
-                <hr />
-                {#each data.accounts as account}
-                    <a href="/quantum/accounts/{account.account_id}/transactions">{account.name}</a>
-                {/each}
-            </div>
-        </div>
-
-        <div data-popup="dataPopupCombobox">
-            <div class="btn-group-vertical variant-filled">
-                {#if $page.params.accountId && $page.route.id?.includes('/transactions')}
-                    <a href={`/quantum/accounts/${$page.params.accountId}/scheduledtransactions`}
-                        >Scheduled Transactions</a
-                    >
-                    <hr />
-                {/if}
-                {#if $page.params.accountId && $page.route.id?.includes('/scheduledtransactions')}
-                    <a href={`/quantum/accounts/${$page.params.accountId}/transactions`}
-                        >Transactions</a
-                    >
-                    <hr />
-                {/if}
-                <a href="/quantum/transactiontypes">Transaction Types</a>
-                <a href="/quantum/payees">Payees</a>
-                <a href="/quantum/categories">Categories</a>
-            </div>
-        </div>
     </svelte:fragment>
 
     <slot />
