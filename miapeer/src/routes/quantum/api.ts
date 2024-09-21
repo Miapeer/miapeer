1451c5f2b50bd9423e18a70947d6280707334ae9
Jep <jeff.navarra@miapeer.com>
2024-09-21 11:44:44 -0600

    Remove unused functions

--- miapeer/src/routes/quantum/api.ts
+++ miapeer/src/routes/quantum/api.ts
@@ -1,392 +1,84 @@
 import { invalidate } from '$app/navigation';
+import { applyAction, deserialize } from '$app/forms';
 
-const createAccount = async ({ portfolioId, accountName, unformattedStartingBalance }) => {
-    if (!portfolioId || !accountName) {
-        return;
-    }
+const deleteAccount = async (targetElement) => {
+    const data = new FormData(targetElement);
 
-    const requestData = {
-        portfolioId,
-        accountName,
-        startingBalance: unformattedStartingBalance || 0
-    };
-    const res = await fetch('/quantum/accounts/new', {
+    const response = await fetch(targetElement.action, {
         method: 'POST',
-        body: JSON.stringify(requestData)
+        body: data
     });
 
-    if (res.ok) {
-        const data = await res.json();
+    /** @type {import('@sveltejs/kit').ActionResult} */
+    const result = deserialize(await response.text());
+
+    if (result.type === 'success') {
         invalidate('quantum:accounts');
-        return data;
     } else {
         console.error('NOT ok');
     }
+
+    applyAction(result);
 };
 
-const updateAccount = async ({
-    portfolioId,
-    accountId,
-    accountName,
-    unformattedStartingBalance
-}) => {
-    if (!portfolioId || !accountId || !accountName) {
-        return;
-    }
+const deleteTransactionType = async (targetElement) => {
+    const data = new FormData(targetElement);
 
-    const requestData = {
-        portfolioId,
-        accountName,
-        startingBalance: unformattedStartingBalance || 0
-    };
-    const res = await fetch(`/quantum/accounts/${accountId}`, {
+    const response = await fetch(targetElement.action, {
         method: 'POST',
-        body: JSON.stringify(requestData)
+        body: data
     });
 
-    if (res.ok) {
-        const data = await res.json();
-        invalidate('quantum:accounts');
-        return data;
-    } else {
-        console.error('NOT ok');
-    }
-};
+    /** @type {import('@sveltejs/kit').ActionResult} */
+    const result = deserialize(await response.text());
 
-const createTransactionType = async ({ portfolioId, transactionTypeName }) => {
-    if (!portfolioId || !transactionTypeName) {
-        return;
-    }
-
-    const requestData = {
-        portfolioId,
-        transactionTypeName
-    };
-    const res = await fetch('/quantum/transactiontypes/new', {
-        method: 'POST',
-        body: JSON.stringify(requestData)
-    });
-
-    if (res.ok) {
-        const data = await res.json();
+    if (result.type === 'success') {
         invalidate('quantum:transactiontypes');
-        return data;
     } else {
         console.error('NOT ok');
     }
+
+    applyAction(result);
 };
 
-const updateTransactionType = async ({ portfolioId, transactionTypeId, transactionTypeName }) => {
-    if (!portfolioId || !transactionTypeId || !transactionTypeName) {
-        return;
-    }
+const deletePayee = async (targetElement) => {
+    const data = new FormData(targetElement);
 
-    const requestData = {
-        portfolioId,
-        transactionTypeName
-    };
-    const res = await fetch(`/quantum/transactiontypes/${transactionTypeId}`, {
+    const response = await fetch(targetElement.action, {
         method: 'POST',
-        body: JSON.stringify(requestData)
+        body: data
     });
 
-    if (res.ok) {
-        const data = await res.json();
-        invalidate('quantum:transactiontypes');
-        return data;
-    } else {
-        console.error('NOT ok');
-    }
-};
+    /** @type {import('@sveltejs/kit').ActionResult} */
+    const result = deserialize(await response.text());
 
-const createPayee = async ({ portfolioId, payeeName }) => {
-    if (!portfolioId || !payeeName) {
-        return;
-    }
-
-    const requestData = {
-        portfolioId,
-        payeeName
-    };
-    const res = await fetch('/quantum/payees/new', {
-        method: 'POST',
-        body: JSON.stringify(requestData)
-    });
-
-    if (res.ok) {
-        const data = await res.json();
+    if (result.type === 'success') {
         invalidate('quantum:payees');
-        return data;
     } else {
         console.error('NOT ok');
     }
+
+    applyAction(result);
 };
 
-const updatePayee = async ({ portfolioId, payeeId, payeeName }) => {
-    if (!portfolioId || !payeeId || !payeeName) {
-        return;
-    }
+const deleteCategory = async (targetElement) => {
+    const data = new FormData(targetElement);
 
-    const requestData = {
-        portfolioId,
-        payeeName
-    };
-    const res = await fetch(`/quantum/payees/${payeeId}`, {
+    const response = await fetch(targetElement.action, {
         method: 'POST',
-        body: JSON.stringify(requestData)
+        body: data
     });
 
-    if (res.ok) {
-        const data = await res.json();
-        invalidate('quantum:payees');
-        return data;
-    } else {
-        console.error('NOT ok');
-    }
-};
+    /** @type {import('@sveltejs/kit').ActionResult} */
+    const result = deserialize(await response.text());
 
-const createCategory = async ({ portfolioId, categoryName }) => {
-    if (!portfolioId || !categoryName) {
-        return;
-    }
-
-    const requestData = {
-        portfolioId,
-        categoryName
-    };
-
-    const res = await fetch('/quantum/categories/new', {
-        method: 'POST',
-        body: JSON.stringify(requestData)
-    });
-
-    if (res.ok) {
-        const data = await res.json();
+    if (result.type === 'success') {
         invalidate('quantum:categories');
-        return data;
     } else {
         console.error('NOT ok');
     }
+
+    applyAction(result);
 };
 
-const updateCategory = async ({ portfolioId, categoryId, categoryName }) => {
-    if (!portfolioId || !categoryId || !categoryName) {
-        return;
-    }
-
-    const requestData = {
-        portfolioId,
-        categoryName
-    };
-
-    const res = await fetch(`/quantum/categories/${categoryId}`, {
-        method: 'POST',
-        body: JSON.stringify(requestData)
-    });
-
-    if (res.ok) {
-        const data = await res.json();
-        invalidate('quantum:categories');
-        return data;
-    } else {
-        console.error('NOT ok');
-    }
-};
-
-const createTransaction = async ({
-    accountId,
-    transactionTypeId,
-    payeeId,
-    categoryId,
-    excludeFromForecast,
-    amount,
-    transactionDate,
-    clearDate,
-    checkNumber,
-    notes
-}) => {
-    if (!accountId) {
-        return;
-    }
-
-    const requestData = {
-        transactionTypeId,
-        payeeId,
-        categoryId,
-        excludeFromForecast,
-        amount,
-        transactionDate,
-        clearDate,
-        checkNumber,
-        notes
-    };
-
-    const res = await fetch(`/quantum/accounts/${accountId}/transactions/new`, {
-        method: 'POST',
-        body: JSON.stringify(requestData)
-    });
-
-    if (res.ok) {
-        const data = await res.json();
-        return data;
-    } else {
-        console.error('NOT ok');
-    }
-};
-
-const updateTransaction = async ({
-    accountId,
-    transactionId,
-    transactionTypeId,
-    payeeId,
-    categoryId,
-    excludeFromForecast,
-    amount,
-    transactionDate,
-    clearDate,
-    checkNumber,
-    notes
-}) => {
-    if (!accountId || !transactionId) {
-        return;
-    }
-
-    const requestData = {
-        transactionTypeId,
-        payeeId,
-        categoryId,
-        excludeFromForecast,
-        amount,
-        transactionDate,
-        clearDate,
-        checkNumber,
-        notes
-    };
-
-    const res = await fetch(`/quantum/accounts/${accountId}/transactions/${transactionId}`, {
-        method: 'POST',
-        body: JSON.stringify(requestData)
-    });
-
-    if (res.ok) {
-        const data = await res.json();
-        return data;
-    } else {
-        console.error('NOT ok');
-    }
-};
-
-const createScheduledTransaction = async ({
-    accountId,
-    transactionTypeId,
-    payeeId,
-    categoryId,
-    fixedAmount,
-    estimateOccurrences,
-    promptDays,
-    startDate,
-    endDate,
-    limitOccurrences,
-    repeatOptionId,
-    onAutopay,
-    notes
-}) => {
-    if (!accountId) {
-        return;
-    }
-
-    const requestData = {
-        accountId,
-        transactionTypeId,
-        payeeId,
-        categoryId,
-        fixedAmount,
-        estimateOccurrences,
-        promptDays,
-        startDate,
-        endDate,
-        limitOccurrences,
-        repeatOptionId,
-        onAutopay,
-        notes
-    };
-
-    const res = await fetch(`/quantum/accounts/${accountId}/scheduledtransactions/new`, {
-        method: 'POST',
-        body: JSON.stringify(requestData)
-    });
-
-    if (res.ok) {
-        const data = await res.json();
-        return data;
-    } else {
-        console.error('NOT ok');
-    }
-};
-
-const updateScheduledTransaction = async ({
-    accountId,
-    scheduledTransactionId,
-    transactionTypeId,
-    payeeId,
-    categoryId,
-    fixedAmount,
-    estimateOccurrences,
-    promptDays,
-    startDate,
-    endDate,
-    limitOccurrences,
-    repeatOptionId,
-    onAutopay,
-    notes
-}) => {
-    if (!accountId || !scheduledTransactionId) {
-        return;
-    }
-
-    const requestData = {
-        accountId,
-        transactionTypeId,
-        payeeId,
-        categoryId,
-        fixedAmount,
-        estimateOccurrences,
-        promptDays,
-        startDate,
-        endDate,
-        limitOccurrences,
-        repeatOptionId,
-        onAutopay,
-        notes
-    };
-
-    const res = await fetch(
-        `/quantum/accounts/${accountId}/scheduledtransactions/${scheduledTransactionId}`,
-        {
-            method: 'POST',
-            body: JSON.stringify(requestData)
-        }
-    );
-
-    if (res.ok) {
-        const data = await res.json();
-        return data;
-    } else {
-        console.error('NOT ok');
-    }
-};
-
-export {
-    createAccount,
-    updateAccount,
-    createTransactionType,
-    updateTransactionType,
-    createPayee,
-    updatePayee,
-    createCategory,
-    updateCategory,
-    createTransaction,
-    updateTransaction,
-    createScheduledTransaction,
-    updateScheduledTransaction
-};
+export { deleteAccount, deleteTransactionType, deletePayee, deleteCategory };
